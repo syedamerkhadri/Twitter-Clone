a6c9d66cf68b4458f1d36d2d1f58ef96e635447f
syed amer khadri <syedamer92@gmail.com>
2022-02-07 17:36:29 -0500

    First Update

---
+++ posts/models.py
@@ -0,0 +1,27 @@
+import cloudinary
+from django.db import models
+# from django.db.models.fields import CharField
+from cloudinary.models import CloudinaryField
+
+
+
+# Create your models here.
+class Post(models.Model):
+    class Meta(object):
+        db_table = 'post'
+
+    name = models.CharField(
+        'Name', blank=False, null=False, max_length=50, db_index=True, default='Anonymous'
+    )
+    body = models.CharField(
+        'Body', blank=True, null=True, max_length=140, db_index=True
+    )
+    created_at = models.DateTimeField(
+        'Created DateTime', blank=True, auto_now_add=True
+    )
+    image = CloudinaryField(
+        'image', blank=True, db_index=True
+    )
+    likecount = models.IntegerField(
+        'likecount', default=0, blank=True, db_index=True
+    )
